e87b4d0738431f067db6fdf6cb93714925bb6aec
amitt <amittomar991@gmail.com>
2023-10-03 11:17:03 +0530

    Initial commit

---
+++ restapi-demo/src/main/java/com/firstspring/restapidemo/controller/CloudAPIService.java
@@ -0,0 +1,12 @@
+package com.firstspring.restapidemo.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //to let Spring know,it's a controller class.
+public class CloudAPIService {
+
+
+
+}
+
---
+++ restapi-demo/src/test/java/com/firstspring/restapidemo/RestapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspring.restapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
